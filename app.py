fcb18d98525855efd5dfda69d510bf4d3dbf5fd0
Ureshipan <tiptevlad@yandex.ru>
2024-12-27 17:56:49 +0300

    refactor: start of create-playlist function + add and delete song in playlist functions done

--- app.py
+++ app.py
@@ -70,16 +70,110 @@ async def get_playlists(request: Request):
         elif data['pass'] == users[name]['pass']:
             return {'mes': 'OK', 'playlists': users[name]['playlists']}
         else:
-            return {'mes': 'Wrong pass'}     
+            return {'mes': 'Wrong pass'}
     else:
         return {'mes': 'Bad request'}
     
 @app.post('/add-to-playlist')
-def add_to_playlist(request: Request):
+async def add_to_playlist(request: Request):
+    global users
     data = await request.json()
     if 'name' in data.keys() and 'pass' in data.keys():
-    pass
-        
+        name = data['name']
+        if name not in users.keys():
+            users[name] = {
+                'pass': data['pass'],
+                'playlists': {
+                    'Beloved': []
+                }
+            }
+            save_users_data(users)
+        if data['pass'] == users[name]['pass']:
+            if 'song' in data.keys():
+                if 'playlist' in data.keys():
+                    if data['playlist'] in users[name]['playlists']:
+                        users[name]['playlists'][data['playlist']].append(data['song'])
+                        save_users_data(users)
+                        logging.info(f'Song "{data['song']}" added to playlist "{data['playlist']}" by user {name}')
+                        return {'mes': 'OK'}
+                    else:
+                        logging.info(f'User {name} tryed to add song "{data['song']}" to non-existed playlist')
+                        return {'mes': 'Playlist doesnt exist'}
+                else:
+                    logging.info(f'User {name} tryed to add song "{data['song']}" without playlist')
+                    return {'mes': 'Playlist doesnt specified in request'}
+            else:
+                logging.info(f'User {name} tryed to add song without song name')
+                return {'mes': 'Song doesnt specified in request'}
+        else:
+            logging.info{f'User {name} tryed add song with wrong pass'}
+            return {'mes': 'Wrong pass'}
+    else:
+        return {'mes': 'Bad request'}
+
+
+@app.post('/delete-from-playlist')
+async def add_to_playlist(request: Request):
+    global users
+    data = await request.json()
+    if 'name' in data.keys() and 'pass' in data.keys():
+        name = data['name']
+        if name not in users.keys():
+            users[name] = {
+                'pass': data['pass'],
+                'playlists': {
+                    'Beloved': []
+                }
+            }
+            save_users_data(users)
+            return {'mes': 'Bro you are new user'}
+        if data['pass'] == users[name]['pass']:
+            if 'song' in data.keys():
+                if 'playlist' in data.keys():
+                    if data['playlist'] in users[name]['playlists']:
+                        if data['song'] in users[name]['playlists'][data['playlist']]:
+                            users[name]['playlists'][data['playlist']].remove(data['song'])
+                            save_users_data(users)
+                            logging.info(f'Song "{data['song']}" deleted from playlist "{data['playlist']}" by user {name}')
+                            return {'mes': 'OK'}
+                        else:
+                            logging.info(f'User {name} tryed to delete song "{data['song']}" from playlist "{data['playlist']}" but there is no that song')
+                            return {'mes': 'Song is not in that playlist'}
+                    else:
+                        logging.info(f'User {name} tryed to add song "{data['song']}" to non-existed playlist')
+                        return {'mes': 'Playlist doesnt exist'}
+                else:
+                    logging.info(f'User {name} tryed to add song "{data['song']}" without playlist')
+                    return {'mes': 'Playlist doesnt specified in request'}
+            else:
+                logging.info(f'User {name} tryed to add song without song name')
+                return {'mes': 'Song doesnt specified in request'}
+        else:
+            logging.info{f'User {name} tryed add song with wrong pass'}
+            return {'mes': 'Wrong pass'}
+    else:
+        return {'mes': 'Bad request'}
+
+
+@app.post('/create-playlist')
+async def create_playlist(request: Request):
+    global users
+    data = await request.json()
+    if 'name' in data.keys() and 'pass' in data.keys():
+        name = data['name']
+        if name not in users.keys():
+            users[name] = {
+                'pass': data['pass'],
+                'playlists': {
+                    'Beloved': []
+                }
+            }
+            save_users_data(users)
+        if data['pass'] == users[name]['pass']:
+            if 'playlist' in data.keys():
+                
+
+
 @app.get('/kys')
 def kill(request: Request):
     os.kill(os.getpid(), signal.SIGKILL)
